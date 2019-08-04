1f5985933b658e8c83ca1332de425d9773da6ab0
hsz <hsz1273327@gmail.com>
2019-08-05 07:01:48 +0800

    update to v1.0.1

--- build.sh
+++ build.sh
@@ -2,5 +2,8 @@ bash make.sh linux64
 source version
 docker build -t hsz1273327/jwtrpc:$PROJECT_VERSION -t hsz1273327/jwtrpc:latest .
 docker push hsz1273327/jwtrpc
+git add .
+git submit -m "update to v$PROJECT_VERSION"
+git push
 git tag v$PROJECT_VERSION
 git push origin --tags
