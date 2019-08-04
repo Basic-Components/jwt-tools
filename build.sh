12ca790347426885dfbc1e57919c3cfeefe4063f
hsz1273327 <hsz1273327@gmail.com>
2019-08-05 07:12:27 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ source version
 docker build -t hsz1273327/jwtrpc:$PROJECT_VERSION -t hsz1273327/jwtrpc:latest .
 docker push hsz1273327/jwtrpc
 git add .
-git submit -m "update to v$PROJECT_VERSION"
+git commit -m "update to v$PROJECT_VERSION"
 git push
 git tag v$PROJECT_VERSION
 git push origin --tags
