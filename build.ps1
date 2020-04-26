1e429394e055bdf40fa89d898ec5f19b975f110e
hsz1273327 <hsz1273327@gmail.com>
2020-04-26 18:06:42 +0800

    update to v2.0.0

--- build.ps1
+++ build.ps1
@@ -1,9 +1,9 @@
 ./make.ps1 linux64
 $env:PROJECT_VERSION="2.0.0"
-docker build -t hsz1273327/jwtcenter:$PROJECT_VERSION -t hsz1273327/jwtcenter:latest .
+docker build -t hsz1273327/jwtcenter:$env:PROJECT_VERSION -t hsz1273327/jwtcenter:latest .
 docker push hsz1273327/jwtcenter
 git add .
-git commit -m "update to v$PROJECT_VERSION"
+git commit -m "update to v$env:PROJECT_VERSION"
 git push
-git tag v$PROJECT_VERSION
+git tag v$env:PROJECT_VERSION
 git push origin --tags
